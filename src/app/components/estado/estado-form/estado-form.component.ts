8bd2a3e56f3ae37bb6738773d8fc760193f0010d
Janio <janiojunior@gmail.com>
2025-03-08 11:26:14 -0300

    Inicio de um CRUD de Estado

---
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EstadoService } from '../../../services/estado.service';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-estado-form',
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule,
+    MatButtonModule, MatToolbarModule, MatIconModule],
+  templateUrl: './estado-form.component.html',
+  styleUrl: './estado-form.component.css'
+})
+export class EstadoFormComponent {
+  formGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private estadoService: EstadoService,
+    private router: Router) {
+
+    this.formGroup = this.formBuilder.group({
+      nome: ['', Validators.required],
+      sigla: ['', Validators.required],
+    })
+
+  }
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      const novoEstado = this.formGroup.value;
+      this.estadoService.insert(novoEstado).subscribe({
+        next: (estadoCadastrado) => {
+          console.log(JSON.stringify(estadoCadastrado));
+          //this.router.navigateByUrl('/estados');
+        },
+        error: (e) => {
+          console.log('Erro ao gravar' + JSON.stringify(e));
+        }
+      })
+    }
+  }
+
+}
--- src/app/components/estado/estado-list/estado-list.component.ts
+++ src/app/components/estado/estado-list/estado-list.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Estado } from '../../../models/estado.model';
 import { EstadoService } from '../../../services/estado.service';
 import { NgFor } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-estado-list',
-  imports: [NgFor],
+  imports: [NgFor, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule],
   templateUrl: './estado-list.component.html',
   styleUrl: './estado-list.component.css'
 })
 export class EstadoListComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'nome', 'sigla', 'acao'];
   estados: Estado[] = [];
 
-  constructor(private estadoService: EstadoService) {}
+  constructor(private estadoService: EstadoService) { }
 
   ngOnInit(): void {
-    this.estadoService.getEstados().subscribe(data => {
+    this.estadoService.findAll().subscribe(data => {
       this.estados = data;
     });
   }
-  
+
 }
