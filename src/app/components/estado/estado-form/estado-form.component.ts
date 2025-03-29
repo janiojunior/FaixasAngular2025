42319f838e0c78c247e07bf0b8989d7b33425156
Janio <janiojunior@gmail.com>
2025-03-29 10:34:22 -0300

    Validacao back e front

--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { EstadoService } from '../../../services/estado.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -10,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { Estado } from '../../../models/estado.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -31,37 +32,56 @@ export class EstadoFormComponent {
 
     this.formGroup = this.formBuilder.group({
       id: [(estado && estado.id) ? estado.id : null],
-      nome: [(estado && estado.nome) ? estado.nome : '', Validators.required],
-      sigla: [(estado && estado.sigla) ? estado.sigla : '', Validators.required],
+      nome: [(estado && estado.nome) ? estado.nome : '', 
+              Validators.compose([Validators.required, 
+                  Validators.maxLength(60)])],
+      sigla: [(estado && estado.sigla) ? estado.sigla : '', 
+              Validators.compose([Validators.required, 
+                  Validators.minLength(2), 
+                  Validators.maxLength(2)])],
     })
 
   }
 
   salvar() {
+    this.formGroup.markAllAsTouched();
+
     if (this.formGroup.valid) {
       const estado = this.formGroup.value;
 
-      if(estado.id == null) {
-        this.estadoService.insert(estado).subscribe({
-          next: (estadoCadastrado) => {
-            this.router.navigateByUrl('/estados');
-          },
-          error: (errorResponse) => {
-            console.log('Erro ao gravar' + JSON.stringify(errorResponse));
-          }
-        })
-      } else {
-        this.estadoService.update(estado).subscribe({
-          next: () => {
-            this.router.navigateByUrl('/estados');
-          },
-          error: (errorResponse) => {
-            console.log('Erro ao gravar' + JSON.stringify(errorResponse));
-          }
-        })
-      }
+      const operacao = estado.id == null
+      ? this.estadoService.insert(estado)
+      : this.estadoService.update(estado);
+
+      operacao.subscribe({
+        next: () => {
+          this.router.navigateByUrl('/estados');
+        },
+        error: (errorResponse) => {
+          console.log('Erro ao gravar' + JSON.stringify(errorResponse));
+          this.tratarErros(errorResponse);
+        }
+      })
     }
   }
+
+  tratarErros(httpError: HttpErrorResponse): void {
+
+    if (httpError.status === 400) {
+      if(httpError.error?.errors){
+        httpError.error.errors.forEach((validationError: any)  => {
+          const formControl = this.formGroup.get(validationError.fieldName);
+          if (formControl) {
+            formControl.setErrors({apiError: validationError.message})
+          }
+        });
+      }
+    } else {
+      alert(httpError.error?.message || "Erro não mapeado do servidor.");
+    }
+
+  }
+
   excluir() {
     const estado = this.formGroup.value;
     if(estado.id != null) {
@@ -75,5 +95,37 @@ export class EstadoFormComponent {
       })
     }
   }
+  getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined) : string {
+    if (!errors || !this.errorMessages[controlName]) {
+      return 'invalid field';
+    }
 
-}
+    for(const errorName in errors) {
+      console.log(errorName);
+      if (this.errorMessages[controlName][errorName]){
+        return this.errorMessages[controlName][errorName];
+      }
+    }
+    return 'invalid field';
+  }
+
+  // é proximno ao Map do java
+  errorMessages: {[controlName: string] : {[errorName: string] : string}} = {
+    nome: {
+      required: 'O nome deve ser informado.',
+      minlength: 'O nome deve ter no mínimo 2 caracteres. ',
+      maxlength: 'O nome deve ter no máximo 60 caracteres. ',
+      apiError: ' '
+    },
+
+    sigla: {
+      required: 'A sigla deve ser informada.',
+      minlength: 'A sigla deve ter no mínimo 2 caracteres. ',
+      maxlength: 'A sigla deve ter no máximo 2 caracteres. ',
+      apiError: ' '
+    },
+  }
+
+  }
+
+
