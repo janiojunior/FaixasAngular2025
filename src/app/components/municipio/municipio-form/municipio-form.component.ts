30c87e8fb3d7b230b706e68a48637fe99a72185b
Janio <janiojunior@gmail.com>
2025-04-05 08:26:20 -0300

    Estrutura de municipio

---
+++ src/app/components/municipio/municipio-form/municipio-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-municipio-form',
+  imports: [],
+  templateUrl: './municipio-form.component.html',
+  styleUrl: './municipio-form.component.css'
+})
+export class MunicipioFormComponent {
+
+}
---
+++ src/app/components/municipio/municipio-list/municipio-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-municipio-list',
+  imports: [],
+  templateUrl: './municipio-list.component.html',
+  styleUrl: './municipio-list.component.css'
+})
+export class MunicipioListComponent {
+
+}
